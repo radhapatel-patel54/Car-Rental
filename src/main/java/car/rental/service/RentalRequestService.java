b8ed4630701dc2c9cecd2a23b6c259d33b2152f5
Radha Singh <radhapatel.patel54@gmail.com>
2019-07-18 21:13:54 -0700

    Comments added in service class

--- src/main/java/car/rental/service/RentalRequestService.java
+++ src/main/java/car/rental/service/RentalRequestService.java
@@ -14,44 +14,45 @@ public class RentalRequestService {
 	@Autowired
 	CarRentalRepo carRentalRepo;
 
-	
-
 	public String calculateExpense(TripInfo tripInfo) throws Exception {
-		
+
 		Vehicle vehicle;
-		CityDistance cityDistance= new CityDistance();
-		
+		CityDistance cityDistance;
 		double basefare = 15;
 		double totalDistance;
-		
 		String result;
-		vehicle=carRentalRepo.getVehicle(tripInfo.getVehicleId());
-		cityDistance=carRentalRepo.getDistance(tripInfo.getFromCity(), tripInfo.getToCity());
-		totalDistance=cityDistance.getDistance();
+
+		vehicle = carRentalRepo.getVehicle(tripInfo.getVehicleId());
+		cityDistance = carRentalRepo.getDistance(tripInfo.getFromCity(), tripInfo.getToCity());
+
+		totalDistance = cityDistance.getDistance();
+
+		/* If it is round trip then multiply with 2*/
+		if (tripInfo.isRoundTrip())
+			totalDistance = totalDistance * 2;
 		
-		if(tripInfo.isRoundTrip()) 
-			totalDistance=totalDistance*2;
-		if(tripInfo.getFuel().equals("Diesel"))
-			basefare -=1;
-		result = "Total Km "+totalDistance+", Basefare : "+totalDistance*basefare;
-		
-		if(tripInfo.isAC()) {
-			basefare +=2;
-			result = result +", AC Charges : "+ totalDistance*2;
+		/* If fuel used is diesel than reduce basefare to 1*/
+		if (tripInfo.getFuel().equals("Diesel"))
+			basefare -= 1;
+		result = "Total Km " + totalDistance + ", Basefare : " + totalDistance * basefare;
+
+		/*If customer use AC than increase basefare by 2 rupee */
+		if (tripInfo.isAC()) {
+			basefare += 2;
+			result = result + ", AC Charges : " + totalDistance * 2;
 		}
-		
-		if(tripInfo.getNoOfPassanger()> vehicle.getLimit()) {
-			basefare = basefare + (tripInfo.getNoOfPassanger()- vehicle.getLimit());
-			result = result +", Extra Passanger Charges : "+ totalDistance*(tripInfo.getNoOfPassanger()- vehicle.getLimit());
+
+		/*If passangers are more than the max limit than increase the fare by 1 rs/km for each extra passangers*/
+		if (tripInfo.getNoOfPassanger() > vehicle.getLimit()) {
+			basefare = basefare + (tripInfo.getNoOfPassanger() - vehicle.getLimit());
+			result = result + ", Extra Passanger Charges : "
+					+ totalDistance * (tripInfo.getNoOfPassanger() - vehicle.getLimit());
 		}
+
+		result = result + " , ------ Total : " + basefare * totalDistance;
 		
-		result =result+" , ------ Total : "+ basefare*totalDistance;
-		//return String.valueOf(basefare*cityDistance.getDistance());
 		return result;
-		
-	
+
 	}
-	
-	
 
 }
